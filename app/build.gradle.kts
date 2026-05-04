97b099472db21f9bd390acbb1b44503aedbdc3f8
SuzannaU <suzanna.ursulet@gmail.com>
2026-05-04 16:52:39 +0200

    AGP and libs updated to last versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,74 +1,72 @@
 plugins {
-  alias(libs.plugins.androidApplication)
-  alias(libs.plugins.kotlin)
-  alias(libs.plugins.ksp)
-  alias(libs.plugins.hilt)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-  namespace = "com.openclassrooms.hexagonal.games"
-  compileSdk = 34
+    namespace = "com.openclassrooms.hexagonal.games"
+    compileSdk = 37
 
-  defaultConfig {
-    applicationId = "com.openclassrooms.hexagonal.games"
-    minSdk = 24
-    targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
+    defaultConfig {
+        applicationId = "com.openclassrooms.hexagonal.games"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-  composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.11"
-  }
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
-  buildFeatures {
-    compose = true
-  }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
-  //kotlin
-  implementation(platform(libs.kotlin.bom))
+    //kotlin
+    implementation(platform(libs.kotlin.bom))
 
-  //DI
-  implementation(libs.hilt)
-  ksp(libs.hilt.compiler)
-  implementation(libs.hilt.navigation.compose)
+    //DI
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
 
-  //compose
-  implementation(platform(libs.compose.bom))
-  implementation(libs.compose.ui)
-  implementation(libs.compose.ui.graphics)
-  implementation(libs.compose.ui.tooling.preview)
-  implementation(libs.material)
-  implementation(libs.compose.material3)
-  implementation(libs.lifecycle.runtime.compose)
-  debugImplementation(libs.compose.ui.tooling)
-  debugImplementation(libs.compose.ui.test.manifest)
+    //compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.material)
+    implementation(libs.material.icons)
+    implementation(libs.compose.material3)
+    implementation(libs.lifecycle.runtime.compose)
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
 
-  implementation(libs.activity.compose)
-  implementation(libs.navigation.compose)
-  
-  implementation(libs.kotlinx.coroutines.android)
-  
-  implementation(libs.coil.compose)
-  implementation(libs.accompanist.permissions)
+    implementation(libs.activity.compose)
+    implementation(libs.navigation.compose)
 
-  testImplementation(libs.junit)
-  androidTestImplementation(libs.ext.junit)
-  androidTestImplementation(libs.espresso.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.coil.compose)
+    implementation(libs.accompanist.permissions)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
