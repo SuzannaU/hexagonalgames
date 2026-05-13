ba25ef427d8e5cce92c0e1a911a635f4dcaa6461
SuzannaU <suzanna.ursulet@gmail.com>
2026-05-13 19:54:19 +0200

    NotificationService is working, and FCM messages trigger notification when app is running

--- app/src/main/java/com/openclassrooms/hexagonal/games/data/service/NotificationService.kt
+++ app/src/main/java/com/openclassrooms/hexagonal/games/data/service/NotificationService.kt
@@ -1,10 +1,67 @@
 package com.openclassrooms.hexagonal.games.data.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.openclassrooms.hexagonal.games.R
+import com.openclassrooms.hexagonal.games.ui.MainActivity
 
 class NotificationService : FirebaseMessagingService(){
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        message.notification?.let {
+            it.body?.let { msg -> Log.i("TAG", msg) }
+            sendVisualNotification(it)
+        }
+    }
+
+    private fun sendVisualNotification(notification: RemoteMessage.Notification) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val requestCode = 0
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val channelId = "fcm_default_channel"
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Firebase Messages",
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationId = 0
+        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 }
